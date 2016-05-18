5e1fe1e2e68a7fa6bb7f8f940c00884998ad0383
xuyunhao <1336209466@qq.com>
2016-05-18 19:16:18 +0800

    增加APPKey

---
+++ src/cn/ucai/superwechar/activity/EditActivity.java
@@ -0,0 +1,36 @@
+package cn.ucai.superwechar.activity;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends BaseActivity{
+	private EditText editText;
+
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		setContentView(cn.easemob.chatuidemo.R.layout.activity_edit);
+		
+		editText = (EditText) findViewById(cn.easemob.chatuidemo.R.id.edittext);
+		String title = getIntent().getStringExtra("title");
+		String data = getIntent().getStringExtra("data");
+		if(title != null)
+			((TextView)findViewById(cn.easemob.chatuidemo.R.id.tv_title)).setText(title);
+		if(data != null)
+			editText.setText(data);
+		editText.setSelection(editText.length());
+		
+	}
+	
+	
+	public void save(View view){
+		setResult(RESULT_OK,new Intent().putExtra("data", editText.getText().toString()));
+		finish();
+	}
+}
