887d0ecb274f8eb70d2c2af5b7ac0f2eac6a8673
xuyunhao <1336209466@qq.com>
2016-05-19 15:09:28 +0800

    添加了equals and hashCode方法

--- src/cn/ucai/superwechar/bean/Member.java
+++ src/cn/ucai/superwechar/bean/Member.java
@@ -9,6 +9,31 @@ public class Member extends User implements java.io.Serializable {
 
 	// Fields
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!super.equals(o)) return false;
+
+		Member member = (Member) o;
+
+		if (!mmemberUserId.equals(member.mmemberUserId)) return false;
+		if (!mmemberUserName.equals(member.mmemberUserName)) return false;
+		if (!mmemberGroupId.equals(member.mmemberGroupId)) return false;
+		return mmemberGroupHxid.equals(member.mmemberGroupHxid);
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + mmemberUserId.hashCode();
+		result = 31 * result + mmemberUserName.hashCode();
+		result = 31 * result + mmemberGroupId.hashCode();
+		result = 31 * result + mmemberGroupHxid.hashCode();
+		return result;
+	}
+
 	/**
 	 * 
 	 */
